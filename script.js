6648e936e53def24fd859166e24ad718bf5d28de
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-09-17 11:17:27 +0300

    finish project

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ const addTodo = (e) => {
   } else if (e.key === "Enter" || e.key === undefined) {
     console.log("Başarılı");
     addTodoUI(newTodo);
+    setTodoLocalStorage(newTodo);
   }
 };
 
 const deleteTodo = (e) => {
-  console.log(e.target.parentElement.parentElement);
   if (e.target.className === "fa-sharp fa-solid fa-trash p-2") {
     e.target.parentElement.parentElement.remove();
+    deleteTodoStorage(e.target.parentElement.parentElement.textContent);
   }
 };
 
@@ -36,5 +37,45 @@ const addTodoUI = (newTodo) => {
   input.value = "";
 };
 
+// localStorage
+
+const getTodos = () => {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  return todos;
+};
+
+const setTodoLocalStorage = (newTodo) => {
+  let todo = getTodos();
+  todo.push(newTodo);
+  localStorage.setItem("todos", JSON.stringify(todo));
+};
+
+const loadAllTodos = () => {
+  let todos = getTodos();
+
+  todos.forEach((todo) => {
+    addTodoUI(todo);
+  });
+};
+
+const deleteTodoStorage = (deleteTodo) => {
+  let todos = getTodos();
+
+  todos.forEach((todo, idx) => {
+    if (todo === deleteTodo) {
+      todos.splice(idx, 1);
+    }
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+document.addEventListener("DOMContentLoaded", loadAllTodos);
+input.addEventListener("keyup", addTodo);
 buttons.addEventListener("click", addTodo);
 delete_button.addEventListener("click", deleteTodo);
